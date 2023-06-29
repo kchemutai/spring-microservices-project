085fe17fc48995eebe1ebd60ddc61eaa766fd0a8
Kevin chemutai <kevinchemutai@Kevins-MacBook-Air.local>
2023-06-29 18:07:24 -0500

    Add discovery server and enable discovery clients

--- customer/src/main/java/com/amigoscode/customer/CustomerService.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -22,7 +22,7 @@ public class CustomerService {
         //todo: check if fraudster
         customerRepository.saveAndFlush(customer);
         FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http://localhost:8082/api/v1/fraud-check/{customerId}",
+                "http://FRAUD/api/v1/fraud-check/{customerId}",
                     FraudCheckResponse.class,
                     customer.getId()
         );
