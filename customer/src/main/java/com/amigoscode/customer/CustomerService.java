30a7158b783ddc2d3ae8db2a6335578c9e130554
Kevin chemutai <kevinchemutai@Kevins-MacBook-Air.local>
2023-06-30 17:41:34 -0500

    add feign client and configure customer and fraud to use it

--- customer/src/main/java/com/amigoscode/customer/CustomerService.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -1,5 +1,7 @@
 package com.amigoscode.customer;
 
+import com.amigoscode.clients.fraud.FraudCheckResponse;
+import com.amigoscode.clients.fraud.FraudClient;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -9,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 public class CustomerService {
     private final CustomerRepository customerRepository;
     private final RestTemplate restTemplate;
+    private final FraudClient fraudClient;
 
     public void registerCustomer(CustomerRegistrationRequest registrationRequest) {
         Customer customer=Customer.builder()
@@ -21,11 +24,7 @@ public class CustomerService {
         //todo: check if email is not taken
         //todo: check if fraudster
         customerRepository.saveAndFlush(customer);
-        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http://FRAUD/api/v1/fraud-check/{customerId}",
-                    FraudCheckResponse.class,
-                    customer.getId()
-        );
+        FraudCheckResponse fraudCheckResponse = fraudClient.isFraudster(customer.getId());
         if (fraudCheckResponse.isFraudster()){
             throw new IllegalStateException("Fraudster");
         }
